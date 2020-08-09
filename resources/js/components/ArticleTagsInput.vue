46ac0131d9bb8daa93d0946a1590c21c230674c9
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-10 07:04:34 +0900

    fix tag_component

--- resources/js/components/ArticleTagsInput.vue
+++ resources/js/components/ArticleTagsInput.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
+    <input type="hidden" name="tags" :value="tagsJson" />
     <vue-tags-input
       v-model="tag"
       :tags="tags"
+      placeholder="タグを5個まで入力できます"
       :autocomplete-items="filteredItems"
       @tags-changed="newTags => tags = newTags"
     />
@@ -44,6 +46,9 @@ export default {
       return this.autocompleteItems.filter(i => {
         return i.text.toLowerCase().indexOf(this.tag.toLowerCase()) !== -1;
       });
+    },
+    tagsJson() {
+      return JSON.stringify(this.tags);
     }
   }
 };
