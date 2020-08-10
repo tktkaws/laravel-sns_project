93f3e7044781db6a1da0d762ed6c6aa96c94348f
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-10 14:35:05 +0900

    finish creating follow_function

--- resources/js/components/FollowButton.vue
+++ resources/js/components/FollowButton.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <button class="btn-sm shadow-none border border-primary p-2" :class="buttonColor">
+    <button
+      class="btn-sm shadow-none border border-primary p-2"
+      :class="buttonColor"
+      @click="clickFollow"
+    >
       <i class="mr-1" :class="buttonIcon"></i>
       {{ buttonText }}
     </button>
@@ -13,9 +17,15 @@ export default {
     initialIsFollowedBy: {
       type: Boolean,
       default: false
+    },
+    authorized: {
+      type: Boolean,
+      default: false
+    },
+    endpoint: {
+      type: String
     }
   },
-
   data() {
     return {
       isFollowedBy: this.initialIsFollowedBy
@@ -30,6 +40,25 @@ export default {
     },
     buttonText() {
       return this.isFollowedBy ? "フォロー中" : "フォロー";
+    }
+  },
+  methods: {
+    clickFollow() {
+      if (!this.authorized) {
+        alert("フォロー機能はログイン中のみ使用できます");
+        return;
+      }
+      this.isFollowedBy ? this.unfollow() : this.follow();
+    },
+    async follow() {
+      const response = await axios.put(this.endpoint);
+
+      this.isFollowedBy = true;
+    },
+    async unfollow() {
+      const response = await axios.delete(this.endpoint);
+
+      this.isFollowedBy = false;
     }
   }
 };
