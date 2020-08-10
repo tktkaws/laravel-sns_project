db9b78705ae57ef512b9f34e88f6c07ddcb6efb0
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-10 15:15:31 +0900

    mash up some blades

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,11 +11,27 @@ class UserController extends Controller
     public function show(string $name)
     {
         $user = User::where('name', $name)->first();
+        $articles = $user->articles->sortByDesc('created_at');
 
         return view('users.show', [
             'user' => $user,
+            'articles' => $articles,
         ]);
     }
+
+    public function likes(string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        $articles = $user->likes->sortByDesc('created_at');
+
+        return view('users.likes', [
+            'user' => $user,
+            'articles' => $articles,
+        ]);
+    }
+
+
 
     public function follow(Request $request, string $name)
     {
--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use App\Mail\BareMail;
 use App\Notifications\PasswordResetNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -45,6 +46,11 @@ class User extends Authenticatable
         $this->notify(new PasswordResetNotification($token, new BareMail()));
     }
 
+    public function articles(): HasMany
+    {
+        return $this->hasMany('App\Article');
+    }
+
     public function followers(): BelongsToMany
     {
         return $this->belongsToMany('App\User', 'follows', 'followee_id', 'follower_id')->withTimestamps();
@@ -53,6 +59,11 @@ class User extends Authenticatable
     public function followings(): BelongsToMany
     {
         return $this->belongsToMany('App\User', 'follows', 'follower_id', 'followee_id')->withTimestamps();
+    }
+
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Article', 'likes')->withTimestamps();
     }
 
     public function isFollowedBy(?User $user): bool
